75a19aef2cc906328e7305e77cb5b188347d13ef
Jesse Garrison <jmgarrison@gmail.com>
2015-07-31 21:40:52 -0700

    Adds project skeleton.

---
+++ web/main.dart
@@ -0,0 +1,13 @@
+library catan_randomizer;
+
+import 'package:angular/angular.dart';
+import 'package:angular/application_factory.dart';
+
+class CatanRandomizerModule extends Module {
+  CatanRandomizerModule() {}
+}
+
+void main() {
+  applicationFactory()
+  .run();
+}
