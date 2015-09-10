0dfd80d78b0213f98ace743ca167a9692d191203
Jesse Garrison <jmgarrison@gmail.com>
2015-09-10 10:23:38 -0700

    [Fixes #1] Fixes transformers for release compilation
    - Adds custom resource url resource wrapper

--- web/main.dart
+++ web/main.dart
@@ -3,6 +3,7 @@ library catan_randomizer;
 import 'package:angular/angular.dart';
 import 'package:angular/application_factory.dart';
 
+import 'package:catan_randomizer/uri/resource_url_resolver_wrapper.dart';
 import 'package:catan_randomizer/component/catan_board/catan_board.dart';
 import 'package:catan_randomizer/component/catan_hex/catan_hex.dart';
 import 'package:catan_randomizer/formatter/slice.dart';
@@ -12,6 +13,8 @@ class CatanRandomizerModule extends Module {
     bind(CatanBoardComponent);
     bind(CatanHexComponent);
     bind(SliceFilter);
+    bind(ResourceResolverConfig, toValue: new ResourceResolverConfig.resolveRelativeUrls(false));
+    bind(ResourceUrlResolver, toImplementation: ResourceUrlResolverWrapper);
   }
 }
 
